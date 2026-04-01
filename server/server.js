d2b6c163befa24dbc8285017c563c98fd40eee24
Jasman Preet Singh <jasmangill22750@gmail.com>
2026-04-01 12:48:05 +0530

    implemented authentication with JWT

--- server/server.js
+++ server/server.js
@@ -1,6 +1,28 @@
 import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
-connectDB();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+app.get("/", (_req, res) => {
+	res.send("API is running");
+});
+
+const startServer = async () => {
+	await connectDB();
+	app.listen(PORT, () => {
+		console.log(`Server running on port ${PORT}`);
+	});
+};
+
+startServer();
